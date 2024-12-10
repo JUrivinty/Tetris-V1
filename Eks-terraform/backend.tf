0bd04f3a8200214c6d10dfaa6b3575676428ba59
JUrivinty <34314804+JUrivinty@users.noreply.github.com>
2024-12-10 04:45:30 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "jaggu-tetris-dec-12102024" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
